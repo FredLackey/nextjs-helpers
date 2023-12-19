ccf7d9a057f3f522fbd61646076954d422bc3618
Fred Lackey <fred.lackey@gmail.com>
2023-12-19 15:52:56 -0500

    v0.0.23

--- src/get-header.mjs
+++ src/get-header.mjs
@@ -1,14 +1,6 @@
-import getHeaders from "./get-headers.mjs";
-
-const getHeader = (request, name, strict = false) => {
+const getHeader = (request, name) => {
   const value = request.headers.get(name);
-  if (value || strict) {
-    return value;
-  }
-  const headers = getHeaders(request);
-  const keys = Object.keys(headers);
-  const key = keys.find(x => x.toLowerCase() === name.toLowerCase());
-  return key ? headers[key] : undefined;
+  return value;
 };
 
 export default getHeader;
