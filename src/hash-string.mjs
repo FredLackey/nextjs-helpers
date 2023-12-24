95f5028359af650b65c711f76fc4ecfa5d21bef2
Fred Lackey <fred.lackey@gmail.com>
2023-12-24 14:55:09 -0500

    v0.0.32

--- src/hash-string.mjs
+++ src/hash-string.mjs
@@ -1,11 +1,13 @@
-import crypto from 'crypto';
-import { STRING_HMAC, DIGEST_OPTION } from './constants.mjs';
+import crypto from "crypto";
+import { STRING_HMAC, DIGEST_OPTION } from "./constants.mjs";
 
-import isValidString from './is-valid-string.mjs';
-import trimToUndefined from './trim-to-undefined.mjs';
+import isValidString from "./is-valid-string.mjs";
+import trimToUndefined from "./trim-to-undefined.mjs";
 
-const hashString = value => {
-  if (!isValidString(value, true)) { return undefined; }
+const hashString = (value) => {
+  if (!isValidString(value, true)) {
+    return undefined;
+  }
   value = trimToUndefined(value);
   return crypto.createHash(STRING_HMAC).update(value).digest(DIGEST_OPTION);
 };
