e313355d37a2fcd082eb8f9ea778f49dcc798e9a
Fred Lackey <fred.lackey@gmail.com>
2024-01-08 04:59:09 -0500

    v0.0.35

--- index.js
+++ index.js
@@ -1,119 +1,111 @@
-export { default as addMinutes } from "./src/add-minutes";
-export { default as cleanAlphanumeric } from "./src/clean-alphanumeric";
-export { default as cleanDigits } from "./src/clean-digits";
-export { default as cleanDto } from "./src/clean-dto";
-export { default as cleanString } from "./src/clean-string";
-export { default as copyObject } from "./src/copy-object";
-export { default as fromDto } from "./src/from-dto";
-export { default as fromEpoch } from "./src/from-epoch";
-export { default as fromIsoDate } from "./src/from-iso-date";
-export { default as fromJson } from "./src/from-json";
-export { default as fromResult } from "./src/from-result";
-export { default as getArrayCount } from "./src/get-array-count";
-export { default as getBlockDate } from "./src/get-blockdate";
-export { default as getBody } from "./src/get-body";
-export { default as getCommonPath } from "./src/get-common-path";
-export { default as getDuration } from "./src/get-duration";
-export { default as getEmail } from "./src/get-email";
-export { default as getEmails } from "./src/get-emails";
-export { default as getFileName } from "./src/get-file-name";
-export { default as getFirst } from "./src/get-first";
-export { default as getHash } from "./src/get-hash";
-export { default as getId } from "./src/get-id";
-export { default as getJwt } from "./src/get-jwt";
-export { default as getLast } from "./src/get-last";
-export { default as getLatestSemver } from "./src/get-latest-semver";
-export { default as getMax } from "./src/get-max";
-export { default as getMin } from "./src/get-min";
-export { default as getPads } from "./src/get-pads";
-export { default as getReactVersion } from "./src/get-react-version";
-export { default as getSingle } from "./src/get-single";
-export { default as getStringSize } from "./src/get-string-size";
-export { default as getSubstring } from "./src/get-substring";
-export { default as getUrlParam } from "./src/get-url-param";
-export { default as getUrlParams } from "./src/get-url-params";
-export { default as hasString } from "./src/has-string";
-export { default as hashString } from "./src/hash-string";
-export { default as hash } from "./src/hash";
-export { default as initArray } from "./src/init-array";
-export { default as isAlpha } from "./src/is-alpha";
-export { default as isAlphanumeric } from "./src/is-alphanumeric";
-export { default as isArray } from "./src/is-array";
-export { default as isAsync } from "./src/is-async";
-export { default as isBoolean } from "./src/is-boolean";
-export { default as isCode } from "./src/is-code";
-export { default as isDate } from "./src/is-date";
-export { default as isDefined } from "./src/is-defined";
-export { default as isDeleted } from "./src/is-deleted";
-export { default as isDigits } from "./src/is-digits";
-export { default as isEmail } from "./src/is-email";
-export { default as isEmpty } from "./src/is-empty";
-export { default as isEqual } from "./src/is-equal";
-export { default as isFunction } from "./src/is-function";
-export { default as isGuidFormat } from "./src/is-guid-format";
-export { default as isIsoDate } from "./src/is-iso-date";
-export { default as isJson } from "./src/is-json";
-export { default as isMatch } from "./src/is-match";
-export { default as isNumber } from "./src/is-number";
-export { default as isObject } from "./src/is-object";
-export { default as isPhoneNumber } from "./src/is-phone-number";
-export { default as isSameDate } from "./src/is-same-date";
-export { default as isSemver } from "./src/is-semver";
-export { default as isString } from "./src/is-string";
-export { default as isUidFormat } from "./src/is-uid-format";
-export { default as isValidArray } from "./src/is-valid-array";
-export { default as isValidChars } from "./src/is-valid-chars";
-export { default as isValidPath } from "./src/is-valid-path";
-export { default as isValidString } from "./src/is-valid-string";
-export { default as isZeroDate } from "./src/is-zero-date";
-export { default as moveDown } from "./src/move-down";
-export { default as moveObject } from "./src/move-object";
-export { default as moveUp } from "./src/move-up";
-export { default as newCode } from "./src/new-code";
-export { default as newGuid } from "./src/new-guid";
-export { default as newSalt } from "./src/new-salt";
-export { default as newUid } from "./src/new-uid";
-export { default as parseJson } from "./src/parse-json";
-export { default as parseJwt } from "./src/parse-jwt";
-export { default as print } from "./src/print";
-export { default as removeDeleted } from "./src/remove-deleted";
-export { default as removePrefix } from "./src/remove-prefix";
-export { default as removeSuffix } from "./src/remove-suffix";
-export { default as respond } from "./src/respond";
-export { default as sort } from "./src/sort";
-export { default as stringify } from "./src/stringify";
-export { default as toAlphanumeric } from "./src/to-alphanumeric";
-export { default as toBoolean } from "./src/to-boolean";
-export { default as toCamelCase } from "./src/to-camel-case";
-export { default as toDigits } from "./src/to-digits";
-export { default as toEpoch } from "./src/to-epoch";
-export { default as toGuidFormat } from "./src/to-guid-format";
-export { default as toKebabCase } from "./src/to-kebab-case";
-export { default as toResult } from "./src/to-result";
-export { default as toSnakeCase } from "./src/to-snake-case";
-export { default as toTable } from "./src/to-table";
-export { default as toUidFormat } from "./src/to-uid-format";
-export { default as trimArray } from "./src/trim-array";
-export { default as trimString } from "./src/trim-string";
-export { default as trimToNull } from "./src/trim-to-null";
-export { default as trimToUndefined } from "./src/trim-to-undefined";
-export { default as uniqueNumbers } from "./src/unique-numbers";
-export { default as uniqueObjects } from "./src/unique-objects";
-export { default as uniqueStrings } from "./src/unique-strings";
-export { default as unique } from "./src/unique";
+import addMinutes from "./src/add-minutes";
+import cleanAlphanumeric from "./src/clean-alphanumeric";
+import cleanDigits from "./src/clean-digits";
+import cleanDto from "./src/clean-dto";
+import cleanString from "./src/clean-string";
+import constants from "./src/constants";
+import copyObject from "./src/copy-object";
+import fromDto from "./src/from-dto";
+import fromEpoch from "./src/from-epoch";
+import fromIsoDate from "./src/from-iso-date";
+import fromJson from "./src/from-json";
+import fromResult from "./src/from-result";
+import getArrayCount from "./src/get-array-count";
+import getBlockDate from "./src/get-blockdate";
+import getBody from "./src/get-body";
+import getCommonPath from "./src/get-common-path";
+import getDuration from "./src/get-duration";
+import getEmail from "./src/get-email";
+import getEmails from "./src/get-emails";
+import getFileName from "./src/get-file-name";
+import getFirst from "./src/get-first";
+import getHash from "./src/get-hash";
+import getId from "./src/get-id";
+import getJwt from "./src/get-jwt";
+import getLast from "./src/get-last";
+import getLatestSemver from "./src/get-latest-semver";
+import getMax from "./src/get-max";
+import getMin from "./src/get-min";
+import getPads from "./src/get-pads";
+import getReactVersion from "./src/get-react-version";
+import getSingle from "./src/get-single";
+import getStringSize from "./src/get-string-size";
+import getSubstring from "./src/get-substring";
+import getUrlParam from "./src/get-url-param";
+import getUrlParams from "./src/get-url-params";
+import hasString from "./src/has-string";
+import hashString from "./src/hash-string";
+import hash from "./src/hash";
+import initArray from "./src/init-array";
+import isAlpha from "./src/is-alpha";
+import isAlphanumeric from "./src/is-alphanumeric";
+import isArray from "./src/is-array";
+import isAsync from "./src/is-async";
+import isBoolean from "./src/is-boolean";
+import isCode from "./src/is-code";
+import isDate from "./src/is-date";
+import isDefined from "./src/is-defined";
+import isDeleted from "./src/is-deleted";
+import isDigits from "./src/is-digits";
+import isEmail from "./src/is-email";
+import isEmpty from "./src/is-empty";
+import isEqual from "./src/is-equal";
+import isFunction from "./src/is-function";
+import isGuidFormat from "./src/is-guid-format";
+import isIsoDate from "./src/is-iso-date";
+import isJson from "./src/is-json";
+import isMatch from "./src/is-match";
+import isNumber from "./src/is-number";
+import isObject from "./src/is-object";
+import isPhoneNumber from "./src/is-phone-number";
+import isSameDate from "./src/is-same-date";
+import isSemver from "./src/is-semver";
+import isString from "./src/is-string";
+import isUidFormat from "./src/is-uid-format";
+import isValidArray from "./src/is-valid-array";
+import isValidChars from "./src/is-valid-chars";
+import isValidPath from "./src/is-valid-path";
+import isValidString from "./src/is-valid-string";
+import isZeroDate from "./src/is-zero-date";
+import moveDown from "./src/move-down";
+import moveObject from "./src/move-object";
+import moveUp from "./src/move-up";
+import newCode from "./src/new-code";
+import newGuid from "./src/new-guid";
+import newSalt from "./src/new-salt";
+import newUid from "./src/new-uid";
+import parseJson from "./src/parse-json";
+import parseJwt from "./src/parse-jwt";
+import print from "./src/print";
+import removeDeleted from "./src/remove-deleted";
+import removePrefix from "./src/remove-prefix";
+import removeSuffix from "./src/remove-suffix";
+import respond from "./src/respond";
+import sort from "./src/sort";
+import stringify from "./src/stringify";
+import toAlphanumeric from "./src/to-alphanumeric";
+import toBoolean from "./src/to-boolean";
+import toCamelCase from "./src/to-camel-case";
+import toDigits from "./src/to-digits";
+import toEpoch from "./src/to-epoch";
+import toGuidFormat from "./src/to-guid-format";
+import toKebabCase from "./src/to-kebab-case";
+import toResult from "./src/to-result";
+import toSnakeCase from "./src/to-snake-case";
+import toTable from "./src/to-table";
+import toUidFormat from "./src/to-uid-format";
+import trimArray from "./src/trim-array";
+import trimString from "./src/trim-string";
+import trimToNull from "./src/trim-to-null";
+import trimToUndefined from "./src/trim-to-undefined";
+import uniqueNumbers from "./src/unique-numbers";
+import uniqueObjects from "./src/unique-objects";
+import uniqueStrings from "./src/unique-strings";
+import unique from "./src/unique";
+import http from "./src/http";
+import jwt from "./src/jwt";
 
-export { default as http } from "./src/http";
-export { default as jwt } from "./src/jwt";
-
-export { default as cleanAlphaNumeric } from "./src/clean-alphanumeric";
-export { default as getSubString } from "./src/get-substring";
-export { default as isAlphaNumeric } from "./src/is-alphanumeric";
-export { default as isPhone } from "./src/is-phone-number";
-export { default as first } from "./src/get-first";
-export { default as last } from "./src/get-last";
-export { default as single } from "./src/get-single";
-
-export {
+const {
   ALPHA,
   ALPHANUMERIC,
   CLEAR_CODE,
@@ -129,6 +121,152 @@ export {
   DECODE_FORMAT,
   STRING_HMAC,
   DEFAULTS,
-} from "./src/constants";
+} = constants;
 
-export { doGet, doPost, doDelete, doPut, ping } from "./src/http";
+const {
+  doGet,
+  doPost,
+  doDelete,
+  doPut,
+  ping,
+} = http;
+
+const namedExports = {
+  addMinutes,
+  cleanAlphanumeric,
+  cleanDigits,
+  cleanDto,
+  cleanString,
+  copyObject,
+  fromDto,
+  fromEpoch,
+  fromIsoDate,
+  fromJson,
+  fromResult,
+  getArrayCount,
+  getBlockDate,
+  getBody,
+  getCommonPath,
+  getDuration,
+  getEmail,
+  getEmails,
+  getFileName,
+  getFirst,
+  getHash,
+  getId,
+  getJwt,
+  getLast,
+  getLatestSemver,
+  getMax,
+  getMin,
+  getPads,
+  getReactVersion,
+  getSingle,
+  getStringSize,
+  getSubstring,
+  getUrlParam,
+  getUrlParams,
+  hasString,
+  hashString,
+  hash,
+  initArray,
+  isAlpha,
+  isAlphanumeric,
+  isArray,
+  isAsync,
+  isBoolean,
+  isCode,
+  isDate,
+  isDefined,
+  isDeleted,
+  isDigits,
+  isEmail,
+  isEmpty,
+  isEqual,
+  isFunction,
+  isGuidFormat,
+  isIsoDate,
+  isJson,
+  isMatch,
+  isNumber,
+  isObject,
+  isPhoneNumber,
+  isSameDate,
+  isSemver,
+  isString,
+  isUidFormat,
+  isValidArray,
+  isValidChars,
+  isValidPath,
+  isValidString,
+  isZeroDate,
+  moveDown,
+  moveObject,
+  moveUp,
+  newCode,
+  newGuid,
+  newSalt,
+  newUid,
+  parseJson,
+  parseJwt,
+  print,
+  removeDeleted,
+  removePrefix,
+  removeSuffix,
+  respond,
+  sort,
+  stringify,
+  toAlphanumeric,
+  toBoolean,
+  toCamelCase,
+  toDigits,
+  toEpoch,
+  toGuidFormat,
+  toKebabCase,
+  toResult,
+  toSnakeCase,
+  toTable,
+  toUidFormat,
+  trimArray,
+  trimString,
+  trimToNull,
+  trimToUndefined,
+  uniqueNumbers,
+  uniqueObjects,
+  uniqueStrings,
+  unique,
+
+  doGet,
+  doPost,
+  doDelete,
+  doPut,
+  ping,
+
+  jwt,
+
+  cleanAlphaNumeric: cleanAlphanumeric,
+  getSubString     : getSubstring,
+  isAlphaNumeric   : isAlphanumeric,
+  isPhone          : isPhoneNumber,
+  first            : getFirst,
+  last             : getLast,
+  single           : getSingle,
+
+  ALPHA,
+  ALPHANUMERIC,
+  CLEAR_CODE,
+  DIGITS,
+  ENUM_NAME,
+  EMPTY_GUID,
+  EMPTY_UID,
+  ZERO_DATE,
+  SALT_OPTION,
+  HMAC_OPTION,
+  DIGEST_OPTION,
+  ENCODE_FORMAT,
+  DECODE_FORMAT,
+  STRING_HMAC,
+  DEFAULTS,
+};
+
+export default namedExports;
