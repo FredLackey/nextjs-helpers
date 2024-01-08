73cbb5769cc972fb89c166b3910c1e451bf56dc3
Fred Lackey <fred.lackey@gmail.com>
2024-01-08 06:48:54 -0500

    v0.0.39

--- src/http.js
+++ src/http.js
@@ -62,8 +62,13 @@ const doPromise = async ({ method = 'GET', url, data, creds = {}, headers = {} }
     const json   = await response.json();
     const status = response.status;
     const ok     = status >= 200 && status < 300;
-    const body   = ok ? json : null;
     const error  = ok ? null : json;
+    const body   = !ok 
+      ? null 
+      : json.data
+        ? {...json.data}
+        : json;
+
     return {
       ok,
       status,
